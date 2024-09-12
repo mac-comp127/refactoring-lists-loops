2fd152fad246a924e7043f86d9054babad23c998
Paul Cantrell <cantrell@pobox.com>
2024-09-12 16:11:14 -0500

    Added starting impls

--- src/listpractice/ListFormatting.java
+++ src/listpractice/ListFormatting.java
@@ -1,5 +1,6 @@
 package listpractice;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -11,27 +12,41 @@ import java.util.List;
 public class ListFormatting {
     /**
      * Returns a new list with each item prefixed with the strings "1. ", "2. ", etc.
-     *
-     * HINT: Use a for-each loop, and create a variable outside the loop to keep
-     *       track of which item number you’re on. (Think: why does the variable
-     *       have to be declared and initialized _outside_ the loop? What
-     *       happens if you move it inside?)
      */
     public static List<String> numberEachItem(List<String> items) {
-        return null;
+        // Here is an implementation that uses a for-each loop, plus a variable to keep track of the
+        // count. (Think: why does `itemNum` have to be declared and initialized _outside_ the loop?
+        // What happens if you move it inside? Try it!)
+
+        int itemNum = 1;
+        List<String> result = new ArrayList<>();
+        for (String item : items) {
+            result.add(itemNum + ". " + item);
+            itemNum++;
+        }
+        return result;
+
+        // TODO: refactor to use C-style loop (see instructions)
     }
 
     /**
      * Formats the items in the given list separated by commas and spaces, e.g.
      * "one, two, three".
-     *
-     * HINT: You can declare a String variable outside the loop, much like you
-     *       declared an into outside the loop in the previous example.
-     *       Initialize it to an empty string, repeatedly add to it inside the
-     *       loop, then return it when you're done.
      */
     public static String formatWithCommas(List<String> items) {
-        return null;
+        String result = "";
+        boolean isFirst = true;
+        for (String item : items) {
+            if (isFirst) {
+                isFirst = false;
+            } else {
+                result += ", ";
+            }
+            result += item;
+        }
+        return result;
+
+        // TODO: refactor to use Java String helper method (see instructions)
     }
 
     /**
@@ -41,13 +56,25 @@ public class ListFormatting {
      *    "one"
      *    "one and two"
      *    "one, two and three"
-     *
-     *  HINT: There is no way to do this without using several conditionals.
-     *        Use the test to help you. After you have it working, see if you
-     *        can simplify your solution at all.
      */
     public static String formatGrammatically(List<String> items) {
-        return null;
+        String result = "";
+        int itemNum = 0;
+        for (String item : items) {
+            if (itemNum > 0) {
+                if (itemNum == items.size() - 1) {
+                    result += " and ";
+                } else {
+                    result += ", ";
+                }
+            }
+            result += item;
+            itemNum++;
+        }
+        return result;
+
+        // TODO: refactor to use Java String methods (see instructions)
+        // TODO: refactor again to use Java C-style for loop (see instructions)
     }
 
     /**
@@ -62,31 +89,22 @@ public class ListFormatting {
      *        help you.
      */
     public static String formatGrammaticallyWithOxfordComma(List<String> items) {
-        return null;
+        String result = "";
+        int itemNum = 0;
+        for (String item : items) {
+            if (itemNum > 0) {
+                if (items.size() > 2) {
+                    result += ", ";
+                } else {
+                    result += " ";
+                }
+                if (itemNum == items.size() - 1) {
+                    result += "and ";
+                }
+            }
+            result += item;
+            itemNum++;
+        }
+        return result;
     }
-
-    /*
-     * Special challenge, if you are looking for something extra:
-     *
-     * Create the following method that can either use the Oxford comma or not,
-     * depending on its second parameter:
-     *
-     *     public static String formatGrammatically(List<String> items, boolean oxfordComma) {
-     *         ???
-     *     }
-     *
-     * Don't implement it by using the two methods above. Instead, do it the
-     * other way around. Change the methods above so they both use your new one,
-     * like this:
-     *
-     *     public static String formatGrammatically(List<String> items) {
-     *         formatGrammatically(items, false);
-     *     }
-     *
-     *     public static String formatGrammaticallyWithOxfordComma(List<String> items) {
-     *         formatGrammatically(items, true);
-     *     }
-     *
-     * ...and see if all the tests still pass!
-     */
 }
